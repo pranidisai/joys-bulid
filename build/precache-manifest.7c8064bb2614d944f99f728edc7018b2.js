a67e7cdce5a08115746b239167a24a14b5a540be
krishna435 <krishna9985@gmail.com>
2018-10-09 11:08:29 +0530

    adding bulid folder

---
+++ build/precache-manifest.7c8064bb2614d944f99f728edc7018b2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf7fb6a274f63207e3030de1c37242c",
+    "url": "/todo/static/media/pic1.cdf7fb6a.png"
+  },
+  {
+    "revision": "cde89431fb9adb9b3e86",
+    "url": "/todo/static/css/main.a03243e7.chunk.css"
+  },
+  {
+    "revision": "8811b3065b1d6e3ef61e",
+    "url": "/todo/static/js/1.8811b306.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "c70582ace59a21310b39b838b19ef5e6",
+    "url": "/todo/static/media/room.c70582ac.jpg"
+  },
+  {
+    "revision": "f96223f80a36cc259cb9cfa2ef968ad5",
+    "url": "/todo/static/media/logo.f96223f8.png"
+  },
+  {
+    "revision": "cde89431fb9adb9b3e86",
+    "url": "/todo/static/js/main.cde89431.chunk.js"
+  },
+  {
+    "revision": "f3d808ba495da023eabc6bfe57dd3ebd",
+    "url": "/todo/static/media/pic2.f3d808ba.png"
+  },
+  {
+    "revision": "43e3813465f1c70038034fdc5c9acd2d",
+    "url": "/todo/static/media/pic3.43e38134.png"
+  },
+  {
+    "revision": "12a7941255ed60772b540f1b89d38389",
+    "url": "/todo/static/media/home2.12a79412.jpg"
+  },
+  {
+    "revision": "16ec5dc00c8c099c9281afce043358ea",
+    "url": "/todo/static/media/bg.16ec5dc0.jpg"
+  },
+  {
+    "revision": "c6e720183da86884227c1cd86da53240",
+    "url": "/todo/index.html"
+  }
+];
